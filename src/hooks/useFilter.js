cccd1c568dfdb603d5c1934d12a4a0a8739b144d
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-06-08 19:43:48 -0300

    Fix: Comenta useFilter

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,16 +1,16 @@
-import { useEffect, useState, useContext } from 'react';
+// import { useEffect, useState, useContext } from 'react';
 
-function useFilter(filterText) {
-  const { initialData, data, setData } = useContext(PlanetsContext);
-  // console.log(data);
-  const [dataFiltered, setdataFiltered] = useState([]);
-  useEffect(() => {
-    const newData = initialData
-      .filter(({ name }) => name.toLowerCase().includes(filterText));
-    // console.log('newData', newData);
-    setdataFiltered(newData);
-  }, [filterText]);
-  return dataFiltered;
-}
+// function useFilter(filterText) {
+//   const { initialData, data, setData } = useContext(PlanetsContext);
+//   // console.log(data);
+//   const [dataFiltered, setdataFiltered] = useState([]);
+//   useEffect(() => {
+//     const newData = initialData
+//       .filter(({ name }) => name.toLowerCase().includes(filterText));
+//     // console.log('newData', newData);
+//     setdataFiltered(newData);
+//   }, [filterText]);
+//   return dataFiltered;
+// }
 
-export default useFilter;
+// export default useFilter;
